e723522629ed614004703fa67daaf4c4f785b33a
D.Yogesh <yogeshone678@gmail.com>
2024-05-02 03:13:57 +0530

    feat(models): update and add new schema definitions
    Signed-off-by: D.Yogesh <yogeshone678@gmail.com>

---
+++ src/models/Transaction.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+    user: { type: String, ref: 'User' },
+    blockNumber: String,
+    timeStamp: String,
+    hash: String,
+    nonce: String,
+    blockHash: String,
+    transactionIndex: String,
+    from: String,
+    to: String,
+    value: String,
+    gas: String,
+    gasPrice: String,
+    isError: String,
+    txreceipt_status: String,
+    input: String,
+    contractAddress: String,
+    cumulativeGasUsed: String,
+    gasUsed: String,
+    confirmations: String
+});
+
+export const Transaction = mongoose.model('Transaction', transactionSchema);
--- src/models/User.js
+++ src/models/User.js
@@ -1,29 +1,8 @@
-import mongoose from "mongoose"
-
-const transactionSchema = new mongoose.Schema({
-    blockNumber: String,
-    timeStamp: String,
-    hash: String,
-    nonce: String,
-    blockHash: String,
-    transactionIndex: String,
-    from: String,
-    to: String,
-    value: String,
-    gas: String,
-    gasPrice: String,
-    isError: String,
-    txreceipt_status: String,
-    input: String,
-    contractAddress: String,
-    cumulativeGasUsed: String,
-    gasUsed: String,
-    confirmations: String
-});
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     address: { type: String, unique: true, required: true },
-    transactions: [transactionSchema]
+    balance: { type: Number, default: 0 }
 });
 
 export const User = mongoose.model('User', userSchema);
